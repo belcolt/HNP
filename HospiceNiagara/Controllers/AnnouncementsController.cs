87c5310e03e65a65b69d8616c1d6025f0b599434
belcolt - Colton Belanger <belcol91@gmail.com>
2015-02-18 20:48:23 -0500

    Upload

---
+++ HospiceNiagara/Controllers/AnnouncementsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospiceNiagara.Controllers
+{
+    public class AnnouncementsController : Controller
+    {
+        // GET: Announcements
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospiceNiagara/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospiceNiagara.Startup))]
+namespace HospiceNiagara
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
