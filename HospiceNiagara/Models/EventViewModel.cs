89b238e03ce2a9650de394b0ea7837ce27aa5d9e
erpfast <emerydscott@hotmail.com>
2015-03-22 13:50:55 -0400

    Local Push

---
+++ HospiceNiagara/Models/EventViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models
+{
+    public class EventListViewModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayName("Start")]
+        public DateTime? StartDate { get; set; }
+
+        [DisplayName("End")]
+        public DateTime? EndDate { get; set; }
+
+        public string Location { get; set; }
+        [DisplayName("RSVPs")]
+        public string AttendanceCount { get; set; }
+    }
+
+    public class InvitationsSingleViewModel
+    {
+        private List<String> attend;
+        private List<String> notattend;
+        private List<String> noresp;
+
+        public InvitationsSingleViewModel()
+        {
+            attend = new List<string>();
+            notattend = new List<string>();
+            noresp = new List<String>();
+        }
+        public List<string> Attending { get { return attend; } set { attend.Add(value.ToString()); } }
+        public List<string> NotAttending { get { return notattend; } set { notattend.Add(value.ToString()); } }
+        public List<string> NotResponded { get { return noresp; } set { noresp.Add(value.ToString()); } }
+
+        public string EventName { get; set; }
+    }
+}
---
+++ HospiceNiagara/Models/Schedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models
+{
+    public class Schedule
+    {
+        public int ID { get; set; }
+
+        public string Category { get; set; }
+
+        public string Month { get; set; }
+
+        public int Year { get; set; }
+
+        [DisplayName("Resource")]
+        public int ResourceID { get; set; }
+        public Resource Resource { get; set; }
+
+    }
+}
