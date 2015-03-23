af83c105dccc648ccbb0a4402881748a622911f7
belcolt - Colton Belanger <belcol91@gmail.com>
2015-03-23 14:32:51 -0400

    stupid git

--- HospiceNiagara/Controllers/SchedulesController.cs
+++ HospiceNiagara/Controllers/SchedulesController.cs
@@ -9,6 +9,8 @@ using System.Web.Mvc;
 using HospiceNiagara.DAL;
 using HospiceNiagara.Models;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace HospiceNiagara.Controllers
 {
@@ -22,6 +24,23 @@ namespace HospiceNiagara.Controllers
             var schedules = db.Schedules.Include(s => s.Resource);
             return View(schedules.ToList());
         }
+
+        [ActionName("ShowSchedule")]
+        public ActionResult Index(int id)
+        {
+            var schedules = db.Schedules.Include(s => s.Resource);
+            var fileID = db.Resources.Where(i => i.ID == id).Select(i => i).Take(1);
+            var theFile = db.FileStores.Where(f => f.ID == fileID.FirstOrDefault().FileStoreID).SingleOrDefault();
+            var length = theFile.FileContent.Length;
+
+            string string64 = Convert.ToBase64String(theFile.FileContent);
+            ViewBag.Mime = theFile.MimeType;
+            ViewBag.Name = theFile.FileName;
+            ViewBag.FileBits = string64;
+            return View("Index", schedules.ToList());
+        }
+
+
 
         // GET: Schedules/Details/5
         public ActionResult Details(int? id)
@@ -45,6 +64,7 @@ namespace HospiceNiagara.Controllers
             var schedSC = db.ResourceSubCategories.Where(sc => sc.ResourceCategoryID == 8);
             ViewBag.ResourceSubcategory = new SelectList(schedSC, "Name", "Name");
             MonthsDropdown();
+            YearDropDown();
             return View();
         }
 
@@ -53,38 +73,64 @@ namespace HospiceNiagara.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Year")] Schedule schedule,string ResourceSubcategory,string SchedMonth)
+        public ActionResult Create([Bind(Include = "Year")] Schedule schedule, string ResourceSubcategory, string SchedMonth, string SchedYear)
         {
             string mimeType = Request.Files[0].ContentType;
-            string fileName = Path.GetFileName(Request.Files[0].FileName);
-            int fileLength = Request.Files[0].ContentLength;
-            var subCat = db.ResourceSubCategories.Where(rsc => rsc.Name == ResourceSubcategory).Single();
-            schedule.Category = ResourceSubcategory;
-            schedule.Month = SchedMonth;
-            string schedDescript = ResourceSubcategory+": "+@schedule.Month+" "+@schedule.Year;
-            var resource = new Resource {FileDesc=schedDescript, DateAdded = DateTime.Now, ResourceCategoryID = 8,ResourceSubCategory=subCat} ;
-            if (!(fileName == "" || fileLength == 0))
+
+            if (SchedMonth.Length == 0)
             {
-                Stream fileStream = Request.Files[0].InputStream;
-                byte[] fileData = new byte[fileLength];
-                fileStream.Read(fileData, 0, fileLength);
-                resource.FileStore = new FileStore
-                {
-                    FileContent = fileData,
-                    MimeType = mimeType,
-                    FileName = fileName
-                };
-                db.Resources.Add(resource);
-                db.SaveChanges();
-                schedule.Resource=resource;
-            };
+                ModelState.AddModelError("Month", "Please select a month from the dropdown list.");
+            }
+            if (ResourceSubcategory.Length == 0)
+            {
+                ModelState.AddModelError("Resource", "Please select a category from the dropdown list.");
+            }
+            if (SchedYear.Length == 0)
+            {
+                ModelState.AddModelError("Year", "Please select a year from the dropdown list.");
+            }
+
+            if (!mimeType.Contains("image/jpeg"))
+            {
+                ModelState.AddModelError("Resource.FileStore.MimeType", "The schedule must be in JPEG format.");
+            }
             if (ModelState.IsValid)
             {
+
+                string fileName = Path.GetFileName(Request.Files[0].FileName);
+                int fileLength = Request.Files[0].ContentLength;
+
+                schedule.Category = ResourceSubcategory;
+                schedule.Month = SchedMonth;
+                schedule.Year = Convert.ToInt32(SchedYear);
+                string schedDescript = ResourceSubcategory + ": " + @schedule.Month + " " + @schedule.Year;
+                var subCat = db.ResourceSubCategories.Where(rsc => rsc.Name == ResourceSubcategory).Single();
+                var resource = new Resource { FileDesc = schedDescript, DateAdded = DateTime.Now, ResourceCategoryID = 8, ResourceSubCategory = subCat };
+                if (!(fileName == "" || fileLength == 0))
+                {
+                    Stream fileStream = Request.Files[0].InputStream;
+                    byte[] fileData = new byte[fileLength];
+                    fileStream.Read(fileData, 0, fileLength);
+                    resource.FileStore = new FileStore
+                    {
+                        FileContent = fileData,
+                        MimeType = mimeType,
+                        FileName = fileName
+                    };
+                    db.Resources.Add(resource);
+                    db.SaveChanges();
+                    schedule.Resource = resource;
+                };
                 db.Schedules.Add(schedule);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             ViewBag.ResourceID = new SelectList(db.Resources, "ID", "FileDesc", schedule.ResourceID);
+            YearDropDown();
+            MonthsDropdown();
+            var schedSC = db.ResourceSubCategories.Where(sc => sc.ResourceCategoryID == 8);
+            ViewBag.ResourceSubcategory = new SelectList(schedSC, "Name", "Name");
             return View(schedule);
         }
 
@@ -101,6 +147,7 @@ namespace HospiceNiagara.Controllers
                 return HttpNotFound();
             }
             MonthsDropdown();
+            YearDropDown(schedule.Year);
             ViewBag.ResourceID = new SelectList(db.Resources, "ID", "FileDesc", schedule.ResourceID);
             return View(schedule);
         }
@@ -110,15 +157,26 @@ namespace HospiceNiagara.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Category,Year,ResourceID")] Schedule schedule,string SchedMonth)
+        public ActionResult Edit([Bind(Include = "ID,Category,Year,ResourceID")] Schedule schedule, string SchedMonth, string SchedYear)
         {
+            if (SchedYear.Length == 0)
+            {
+                ModelState.AddModelError("Year", "Please select a year from the dropdown list.");
+            }
+            if (SchedMonth.Length == 0)
+            {
+                ModelState.AddModelError("Month", "Please select a month from the dropdown list.");
+            }
             if (ModelState.IsValid)
             {
                 schedule.Month = SchedMonth;
+                schedule.Year = Convert.ToInt32(SchedYear);
                 db.Entry(schedule).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            MonthsDropdown();
+            YearDropDown(schedule.Year);
             ViewBag.ResourceID = new SelectList(db.Resources, "ID", "FileDesc", schedule.ResourceID);
             return View(schedule);
         }
@@ -165,6 +223,22 @@ namespace HospiceNiagara.Controllers
             List<string> months = new List<String>{"January","February","March","April","May","June","July","August",
                 "Sepetember","October","November","December"};
             ViewBag.SchedMonth = new SelectList(months);
+        }
+
+        private void YearDropDown(object selectedYear = null)
+        {
+
+            int thisYear = DateTime.Now.Year;
+            List<string> years = new List<string>();
+            for (int i = 0; i <= 10; i++)
+            {
+
+                years.Add(thisYear.ToString());
+                thisYear++;
+            }
+            ViewBag.SchedYear = new SelectList(years, selectedYear);
+
+
         }
     }
 }
